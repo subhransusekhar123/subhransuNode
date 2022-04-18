67f82e50805e473a903ff600212e4b5310f4f728
subhransu <subhransusekharsahu29@gmail.com>
2022-04-18 12:47:37 +0530

    my first push from vs code

---
+++ index.js
@@ -0,0 +1,8 @@
+// import chalk  from 'chalk' 
+console.log('Package.json')
+// const colors = require('colors');
+const chalk = require('chalk')
+
+console.log(chalk.blue('subhransu'))
+
+// console.log('hello'.green.bgCyan)
